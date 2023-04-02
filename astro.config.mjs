a90c5d1904b54d079ad74c70eb047200f1826414
Jared Connor <jaredconnor301@gmail.com>
2023-04-02 11:49:07 -0700

    Update to tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://jaredtconnor.com",
-  integrations: [tailwind({
-    // Providing custom path to tailwind config
-    config: { path: "./tailwind.config.cjs" }
-  })]
+  integrations: [mdx(), tailwind()],
 });
