8b6c228c4290b3debbfa73a0508efeb34743116c
Jared Connor <jaredconnor301@gmail.com>
2023-04-04 20:54:57 -0700

    Update to project collection and post formatting

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
+const blogCollection = defineCollection({
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
@@ -12,4 +12,19 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const projectCollection = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    // Transform string to Date object
+    date: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+  project: projectCollection,
+};
