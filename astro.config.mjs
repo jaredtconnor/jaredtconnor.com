d707b511456e9f4790f296f26ef7b6de81fcd20a
Jared Connor <jaredconnor301@gmail.com>
2023-04-06 03:58:47 +0000

    Adding wrangler files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import worker, { cloudflare } from 'astro-service-worker/adapter';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,4 +12,6 @@ export default defineConfig({
         applyBaseStyles: false,
       }
     })],
+  output: 'server',
+  adapter: cloudflare()
 });
