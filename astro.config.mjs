66ba9cf4bf76a2cadaaeeba21c8a64cf36fc46a2
Jared Connor <jaredconnor301@gmail.com>
2023-04-05 21:09:50 -0700

    Publishing branch

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    mdx(),
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      }
-    })],
+  integrations: [mdx(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  })],
+  output: "server",
+  adapter: cloudflare()
 });
