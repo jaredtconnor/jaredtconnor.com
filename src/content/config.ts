7c93b206dca7bcdbd7a0bf4a5a931ed5b139356a
Jared Connor <jaredconnor301@gmail.com>
2023-04-24 00:23:37 +0000

    Update to rss feed

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,4 @@
 import { defineCollection, z } from "astro:content";
-import { rssSchema } from "@astrojs/rss";
 
 const blogCollection = defineCollection({
   // Type-check frontmatter using a schema
