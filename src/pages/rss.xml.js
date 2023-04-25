c5f4fc1e56c1021fbcf409716059e80cca2597bb
Jared Connor <jaredconnor301@gmail.com>
2023-04-24 21:13:32 -0700

    Update to rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,29 +3,18 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
 export async function get(context) {
-
-  const allPosts = await getCollection('blog', (post) => {
-    return post;
-  });
-
-  const sortedPosts = allPosts.sort(
-    (a, b) => b.data.date.valueOf() - a.data.date.valueOf()
-  );
-
-  const posts = sortedPosts
-
+  const blog = await getCollection('blog');
   return rss({
-    xmlns: { h: 'http://www.w3.org/TR/html4/' },
-    title: 'my title',
-    description: 'my description',
+    title: 'Buzz’s Blog',
+    description: 'A humble Astronaut’s guide to the stars',
     site: context.site,
-    customData: '<language>en-us</language>',
-    items: posts.map((post) => ({
-      title: `Post ${post.data.id}`,
-      pubDate: post.data.date,
-      description:
-        post.data.description || post.data.excerpt || post.data.content || '',
-      link: `/post/${post.data.id}/`,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      date: post.data.pubDate,
+      // Compute RSS link from post `slug`
+      // This example assumes all posts are rendered as `/blog/[slug]` routes
+      link: `/blog/${post.slug}/`,
     })),
   });
 }
+
