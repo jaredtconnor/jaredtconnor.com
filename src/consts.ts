5ac7b6d85cb10e875378f97a8396b4b4867a438f
Jared Connor <jaredconnor301@gmail.com>
2023-05-12 14:09:13 -0700

    Update to site name

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Atomic Thoughts";
+export const SITE_TITLE = "Over Index";
 export const SITE_DESCRIPTION = "Jared Connor's Website";
