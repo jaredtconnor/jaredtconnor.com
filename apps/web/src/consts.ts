6ce7b2f05fb4fdadfca14763c734bf997ed789cc
Jared Connor <jaredconnor301@gmail.com>
2023-12-20 07:33:46 -0800

    Update to existing

--- apps/web/src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Over Index'
-export const SITE_DESCRIPTION = "Jared Connor's Website"
--- apps/web/src/env.d.ts
+++ apps/web/src/env.d.ts
@@ -1,2 +1,5 @@
-/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly ASTRO_PORT: string;
+}
