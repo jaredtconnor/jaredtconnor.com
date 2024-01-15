667b8942ae4cbdffa50f5abcba4700de06a061c0
Jared Connor <jaredconnor301@gmail.com>
2024-01-15 13:49:28 -0800

    Moving to payload 2.0

--- payload/src/collections/Media.ts
+++ payload/src/collections/Media.ts
@@ -1,27 +1,25 @@
-import { CollectionConfig } from 'payload/types'
+import { CollectionConfig } from "payload/types";
 
 export const Media: CollectionConfig = {
-  slug: 'media',
-  admin: {
-    useAsTitle: 'title',
-  },
+  slug: "media",
+  admin: {},
   access: {
     read: (): boolean => true,
     create: () => true,
     update: () => true,
   },
   upload: {
-    staticURL: '/media',
-    staticDir: 'media',
-    mimeTypes: ['image/*', 'application/*'],
+    staticURL: "/media",
+    staticDir: "media",
+    mimeTypes: ["image/*"],
   },
 
   fields: [
     {
-      name: 'alt',
-      type: 'text',
+      name: "alt",
+      type: "text",
     },
   ],
-}
+};
 
-export default Media
+export default Media;
--- payload/src/payload.config.ts
+++ payload/src/payload.config.ts
@@ -1,28 +1,26 @@
-import { buildConfig } from 'payload/config'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
-import path from 'path'
-import Posts from '@/collections/Posts'
-import Users from '@/collections/Users'
-import Media from '@/collections/Media'
+import { buildConfig } from "payload/config";
+import path from "path";
+import Posts from "@/collections/Posts";
+import Users from "@/collections/Users";
+import Media from "@/collections/Media";
 
 export default buildConfig({
   serverURL: process.env.PAYLOAD_URL,
   admin: {
     user: Users.slug,
-    webpack: config => ({
+    webpack: (config) => ({
       ...config,
       resolve: {
         ...config.resolve,
         alias: {
           ...config.resolve.alias,
-          '@': path.resolve(__dirname, './'),
+          "@": path.resolve(__dirname, "./"),
         },
       },
     }),
   },
   collections: [Posts, Users, Media],
   typescript: {
-    outputFile: path.resolve('/', 'types.ts'),
+    outputFile: path.resolve("/", "types.ts"),
   },
-  editor: lexicalEditor({}),
-})
+});
