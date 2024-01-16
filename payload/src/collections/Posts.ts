a6eb4542808a53725ee55dbc31ba6334bf29e8f8
Jared Connor <jaredconnor301@gmail.com>
2024-01-15 16:53:11 -0800

    Working payload cms 2.0 build

--- payload/src/collections/Posts.ts
+++ payload/src/collections/Posts.ts
@@ -1,98 +1,31 @@
-import { CollectionConfig } from "payload/types";
+import { CollectionConfig } from 'payload/types'
+
 const Posts: CollectionConfig = {
-  slug: "posts",
+  slug: 'posts',
   admin: {
-    defaultColumns: ["title", "author", "status"],
-    useAsTitle: "title",
+    useAsTitle: 'title',
   },
   access: {
     read: () => true,
-    create: () => true,
-    update: () => true,
   },
-  hooks: {
-    afterChange: [
-      async () => {
-        console.log(process.env.TOKEN);
 
-        try {
-          process.env.NODE_ENV !== "development" &&
-            console.log(
-              await fetch(
-                `https://api.github.com/repos/${process.env.REPOSITORY}/dispatches`,
-                {
-                  method: "POST",
-                  headers: {
-                    Accept: "application/vnd.github.everest-preview+json",
-                    Authorization: `token ${process.env.TOKEN}`,
-                  },
-                  body: JSON.stringify({
-                    event_type: "payload_update",
-                  }),
-                }
-              )
-            );
-        } catch (e) {
-          console.log(e);
-        }
-      },
-    ],
-  },
   fields: [
     {
-      name: "title",
-      type: "text",
+      name: 'title',
+      type: 'text',
+      required: true,
     },
     {
-      name: "hallo",
-      type: "text",
+      name: 'content',
+      type: 'text',
+      required: true,
     },
     {
-      name: "publishedDate",
-      type: "date",
-    },
-
-    {
-      name: "content",
-      type: "richText",
-      admin: {
-        elements: ["h2", "h3", "h4", "link", "ol", "ul", "upload"],
-        leaves: ["bold", "italic", "underline"],
-        upload: {
-          collections: {
-            media: {
-              fields: [
-                {
-                  name: "imagel",
-                  type: "upload",
-                  relationTo: "media",
-                  required: true,
-                },
-              ],
-            },
-          },
-        },
-      },
-    },
-    {
-      name: "status",
-      type: "select",
-      options: [
-        {
-          value: "draft",
-          label: "Draft",
-        },
-        {
-          value: "published",
-          label: "Published",
-        },
-      ],
-      defaultValue: "draft",
-      admin: {
-        position: "sidebar",
-      },
+      name: 'slug',
+      type: 'text',
+      required: true,
     },
   ],
-};
+}
 
-export default Posts;
+export default Posts
