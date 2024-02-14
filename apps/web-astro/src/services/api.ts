8095e7159568fc27d93cc03b9589df0c1319b47e
Jared Connor <jaredconnor301@gmail.com>
2024-02-13 21:12:22 -0800

    Adding proc file

--- apps/web-astro/src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import { client as directus } from './directus';
-
-export async function getLatestArticles() {
-  const data = await directus
-    .items('articles')
-    .readByQuery({ limit: -1, filter: { status: 'published' }, sort: ['-date_created'] });
-  return data;
-}
