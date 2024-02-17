ab6123f5ed211d30209e34c06a76a3d594a7e7d2
Jared Connor <jaredconnor301@gmail.com>
2024-02-17 15:44:07 -0800

    Removing all astro-paper content

--- apps/web/src/components/Card.tsx
+++ apps/web/src/components/Card.tsx
@@ -1,15 +1,14 @@
-import { slugifyStr } from "@utils/slugify";
 import Datetime from "./Datetime";
 import type { CollectionEntry } from "astro:content";
 
 export interface Props {
   href?: string;
-  frontmatter: CollectionEntry<"blog">["data"];
+  frontmatter: null
   secHeading?: boolean;
 }
 
 export default function Card({ href, frontmatter, secHeading = true }: Props) {
-  const { title, pubDatetime, modDatetime, description } = frontmatter;
+  //const { title, pubDatetime, modDatetime, description } = frontmatter;
 
   const headerProps = {
     style: { viewTransitionName: slugifyStr(title) },
