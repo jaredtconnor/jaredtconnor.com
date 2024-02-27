fdb039b6e4c7b63adc0f917dbfa3ece1277d56e5
Jared Connor <jaredconnor301@gmail.com>
2024-02-26 16:41:08 -0800

    Pre-Structure-Fix

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -11,53 +11,18 @@ function withOpacity(variableName) {
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [
+    require("@tailwindcss/typography"), 
+    require("postcss-color-scheme/tailwind")
+  ],
   theme: {
     // Remove the following screen breakpoint or add other breakpoints
     // if one breakpoint is not enough for you
     screens: {
       sm: "640px",
     },
-
-    extend: {
-      textColor: {
-        skin: {
-          base: withOpacity("--color-text-base"),
-          accent: withOpacity("--color-accent"),
-          inverted: withOpacity("--color-fill"),
-        },
-      },
-      backgroundColor: {
-        skin: {
-          fill: withOpacity("--color-fill"),
-          accent: withOpacity("--color-accent"),
-          inverted: withOpacity("--color-text-base"),
-          card: withOpacity("--color-card"),
-          "card-muted": withOpacity("--color-card-muted"),
-        },
-      },
-      outlineColor: {
-        skin: {
-          fill: withOpacity("--color-accent"),
-        },
-      },
-      borderColor: {
-        skin: {
-          line: withOpacity("--color-border"),
-          fill: withOpacity("--color-text-base"),
-          accent: withOpacity("--color-accent"),
-        },
-      },
-      fill: {
-        skin: {
-          base: withOpacity("--color-text-base"),
-          accent: withOpacity("--color-accent"),
-        },
-        transparent: "transparent",
-      },
-      fontFamily: {
-        mono: ["IBM Plex Mono", "monospace"],
-      },
-
+    extend: { 
+      darkMode: 'class',
       typography: {
         DEFAULT: {
           css: {
@@ -77,8 +42,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require("@tailwindcss/typography"),
-  ],
 
 };
