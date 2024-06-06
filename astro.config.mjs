b110a867d41e760b380ab9e5620018845030bffc
Jared Connor <jaredconnor301@gmail.com>
2024-06-06 11:57:10 -0700

    Changing deployment to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,22 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
-import cloudflare from "@astrojs/cloudflare";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.jaredtconnor.com",
-  integrations: [mdx(), tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), sitemap()],
-  output: "server",
-  adapter: cloudflare()
-});
+  site: 'https://www.jaredtconnor.com',
+  integrations: [
+    mdx(),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    sitemap(),
+  ],
+  output: 'server',
+  adapter: vercel(),
+})
